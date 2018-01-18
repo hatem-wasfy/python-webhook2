a84b6eae5d0b13c6c3e51581dc3a9bd5d075c7cf
hatem-wasfy <34537566+hatem-wasfy@users.noreply.github.com>
2018-01-18 18:08:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,9 @@ def makeWebhookResult(req):
 
     ###speech = "There are nice" + attraction #+ "places in " + city + "to have" + resolvedQuery + " and I will tell you about them. " #+ str(cost[zone])
     ##working### speech = "There are nice places in " + city + " and I will tell you about them. "
-    speech = "There are nice " + attraction + " to have " + resolvedQuery + "places in " + city + " and I will tell you about them. "
+    #speech = "There are nice " + attraction + " to have " + resolvedQuery + "places in " + city + " and I will tell you about them. "
+    speech = "There are nice places in " + city + attraction + resolvedQuery + " and I will tell you about them. "
+    
     print("Response:")
     print(speech)
     return {
