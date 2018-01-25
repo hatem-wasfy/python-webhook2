3ae914208310c58c2a27e5320037314079a0c9c0
hatem-wasfy <34537566+hatem-wasfy@users.noreply.github.com>
2018-01-25 15:19:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,13 +28,11 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    ##if req.get("result").get("action") != "shipping.cost":
-       ## return {}
+
     result = req.get("result")
     parameters = result.get("parameters")
     city = parameters.get("city")
 
-    #cost = {'Andhra Bank':'6.85%', 'Allahabad Bank':'6.75%', 'Axis Bank':'6.5%', 'Bandhan bank':'7.15%', 'Bank of Maharashtra':'6.50%', 'Bank of Baroda':'6.90%', 'Bank of India':'6.60%', 'Bharatiya Mahila Bank':'7.00%', 'Canara Bank':'6.50%', 'Central Bank of India':'6.60%', 'City Union Bank':'7.10%', 'Corporation Bank':'6.75%', 'Citi Bank':'5.25%', 'DBS Bank':'6.30%', 'Dena Bank':'6.80%', 'Deutsche Bank':'6.00%', 'Dhanalakshmi Bank':'6.60%', 'DHFL Bank':'7.75%', 'Federal Bank':'6.70%', 'HDFC Bank':'5.75% to 6.75%', 'Post Office':'7.10%', 'Indian Overseas Bank':'6.75%', 'ICICI Bank':'6.25% to 6.9%', 'IDBI Bank':'6.65%', 'Indian Bank':'4.75%', 'Indusind Bank':'6.85%', 'J&K Bank':'6.75%', 'Karnataka Bank':'6.50 to 6.90%', 'Karur Vysya Bank':'6.75%', 'Kotak Mahindra Bank':'6.6%', 'Lakshmi Vilas Bank':'7.00%', 'Nainital Bank':'7.90%', 'Oriental Bank of Commerce':'6.85%', 'Punjab National Bank':'6.75%', 'Punjab and Sind Bank':'6.4% to 6.80%', 'Saraswat bank':'6.8%', 'South Indian Bank':'6% to 6.75%', 'State Bank of India':'6.75%', 'Syndicate Bank':'6.50%', 'Tamilnad Mercantile Bank Ltd':'6.90%', 'UCO bank':'6.75%', 'United Bank Of India':'6%', 'Vijaya Bank':'6.50%', 'Yes Bank':'7.10%'}
     resolvedQuery = parameters.get("resolvedQuery")
     attraction = parameters.get("attraction")    
 
@@ -54,7 +52,7 @@ def makeWebhookResult(req):
         "displayText": speech,
         #"data": {},
         #"contextOut": [],
-        ##"source": "BankRates"
+        ##"source": "travelsourse"
     }
 
 if __name__ == '__main__':
