ff0472c3e87d4a75abcf7c251584f3d2bcb1e8c1
hatem-wasfy <34537566+hatem-wasfy@users.noreply.github.com>
2018-02-09 15:08:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -61,13 +61,16 @@ def makeWebhookResult(req):
         place.get_details()
         place_url=place.url
         
+        #wasfy test
+        place_phoo=place.photos
+        
         #Getting place photos
 
-        for photo in place.photos:
+        #for photo in place.photos:
             # 'maxheight' or 'maxwidth' is required
             ###place_photo = photo.get(maxheight=500, maxwidth=500)
             # MIME-type, e.g. 'image/jpeg'
-            place_mime = photo.mimetype
+            #place_mime = photo.mimetype
             # Image URL
             ###place_photo_url=photo.url
             # Original filename (optional)
