ce4406e5f4733c5655a2e6f02e8f084ba15995e8
hatem-wasfy <34537566+hatem-wasfy@users.noreply.github.com>
2018-02-15 16:35:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import urlparse
 ######Database########
 
 urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["postgres:///vxjbtravsqldpn:8e28f8853a5ad91bb6ec8614359773e844b6afbe8e57324bfa90d89c050a3ef8@ec2-54-221-234-62.compute-1.amazonaws.com:5432//dn7coi02b1hab
+url = urlparse.urlparse(os.environ["postgres:\/\/vxjbtravsqldpn:8e28f8853a5ad91bb6ec8614359773e844b6afbe8e57324bfa90d89c050a3ef8\@ec2-54-221-234-62.compute-1.amazonaws.com:5432\/\/dn7coi02b1hab
 "])
                                    
     
